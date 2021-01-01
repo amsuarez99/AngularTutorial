b07f3e038996f639f9349328c9f4f1d0e5dd9a25
Marcelo Suárez <amsuarez99@gmail.com>
2020-12-31 18:22:45 -0600

    adds validation to reactive form

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../../shared/feedback';
 
@@ -12,6 +12,7 @@ export class ContactComponent implements OnInit {
   public feedbackForm!: FormGroup;
   public feedback!: Feedback;
   public contactType = ContactType;
+  @ViewChild('fform') feedbackFormDirective: any;
 
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -22,10 +23,10 @@ export class ContactComponent implements OnInit {
 
   public createForm(): void {
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      telnum: [0, Validators.required],
+      email: ['', Validators.required],
       agree: false,
       contacttype: 'None',
       message: ''
@@ -35,7 +36,16 @@ export class ContactComponent implements OnInit {
   public onSubmit(): void {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+    this.feedbackFormDirective.resetForm();
   }
 
 }
